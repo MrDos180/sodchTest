ccfc3d2a4e328258ba097f3692fd751897f3ea35
MrDos180 <ilya180@yandex.ru>
2019-11-28 12:45:09 +0300

    123

---
+++ src/test/java/ru/Sodch/SodchAction.java
@@ -0,0 +1,38 @@
+package ru.Sodch;
+
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class SodchAction {
+
+
+   public static void loginAction (String sodchUsername, String sodchPassword) {
+       $(By.xpath("//div[.='Логин:']//input")).setValue(sodchUsername);
+       $(By.xpath("//div[.='Пароль:']//input")).setValue(sodchPassword);
+       $(By.xpath("//a[@class='x-btn x-unselectable x-box-item x-toolbar-item x-btn-default-small']")).click();
+       $(By.xpath("//div[@class ='x-component x-box-item x-component-default']")).shouldBe(visible);
+
+
+   }
+
+   public static void createKusp() {
+   $(By.xpath("//div[@class='x-container navigationItem x-container-default x-box-layout-ct']//label[.='Создать КУСП']")).click();
+   $(By.xpath("//div[.='Фамилия:']//input")).shouldBe(visible);
+   $(By.xpath("//div[.='Фамилия:']//input")).setValue("Иванов");
+       $(By.xpath("//div[.='Имя:']//input")).setValue("Иван");
+       $(By.xpath("//div[.='Отчество:']//input")).setValue("Иванович");
+        $(By.xpath("//textarea[@class='x-form-field x-form-required-field x-form-text x-form-text-default x-form-textarea']")).setValue("тест");
+        $(By.xpath("//input[@placeholder='Форма поступления']")).setValue("Анонимное сообщение о террористическом акте");
+        $(By.xpath("//span[.='Сохранить']")).click();
+        String kusp = $(By.xpath("//div[.='№:']//input")).getValue();
+
+
+
+       $(By.xpath("//div[.='КУСП № "+ kusp+" успешно сохранен']")).shouldBe(hidden);
+       $(By.xpath("//span[.='Закрыть']")).click();
+       $(By.xpath("//div[.='"+kusp+"']")).shouldBe(visible);
+
+   }
+}
--- src/test/java/ru/Sodch/TestTest.java
+++ src/test/java/ru/Sodch/TestTest.java
@@ -1,38 +1,30 @@
 package ru.Sodch;
 
-import org.openqa.selenium.By;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
+import static ru.Sodch.SodchAction.createKusp;
+import static ru.Sodch.SodchAction.loginAction;
+import static ru.Sodch.TestSetup.loginSetup;
+
+
 
 public class TestTest {
+    @BeforeClass
+    public static void openSodch() {
+        loginSetup();
+    }
+    @Test
+    public void login() {
+        loginAction("operalex","operalex");
+        createKusp();
 
+    }
 
-   public static void loginAction (String sodchUsername, String sodchPassword) {
-       $(By.xpath("//div[.='Логин:']//input")).setValue(sodchUsername);
-       $(By.xpath("//div[.='Пароль:']//input")).setValue(sodchPassword);
-       $(By.xpath("//a[@class='x-btn x-unselectable x-box-item x-toolbar-item x-btn-default-small']")).click();
-       $(By.xpath("//div[@class ='x-component x-box-item x-component-default']")).shouldBe(visible);
-
-
-   }
-
-   public static void createKusp() {
-   $(By.xpath("//div[@class='x-container navigationItem x-container-default x-box-layout-ct']//label[.='Создать КУСП']")).click();
-   $(By.xpath("//div[.='Фамилия:']//input")).shouldBe(visible);
-   $(By.xpath("//div[.='Фамилия:']//input")).setValue("Иванов");
-       $(By.xpath("//div[.='Имя:']//input")).setValue("Иван");
-       $(By.xpath("//div[.='Отчество:']//input")).setValue("Иванович");
-        $(By.xpath("//textarea[@class='x-form-field x-form-required-field x-form-text x-form-text-default x-form-textarea']")).setValue("тест");
-        $(By.xpath("//input[@placeholder='Форма поступления']")).setValue("Анонимное сообщение о террористическом акте");
-        $(By.xpath("//span[.='Сохранить']")).click();
-        String kusp = $(By.xpath("//div[.='№:']//input")).getValue();
-
-
-
-       $(By.xpath("//div[.='КУСП № "+ kusp+" успешно сохранен']")).shouldBe(hidden);
-       $(By.xpath("//span[.='Закрыть']")).click();
-       $(By.xpath("//div[.='"+kusp+"']")).shouldBe(visible);
-
-   }
+    @AfterClass
+    public static void logout () {
+        close();
+    }
 }
